51903af66bcf795ed07bfc53190ad30dda811681
Sarah B <sarahxcellence@Sarahs-iMac.local>
2017-11-23 15:46:43 +0100

    Initial Commit

---
+++ Anwesenheit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Anwesenheit
+//
+//  Created by Sarah B on 23.11.17.
+//  Copyright © 2017 THM Gießen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Anwesenheit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Anwesenheit
+//
+//  Created by Sarah B on 23.11.17.
+//  Copyright © 2017 THM Gießen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
